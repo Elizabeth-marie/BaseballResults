6943659386b69ac3b197288bd186b7707212ce15
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2019-05-11 22:21:28 -0600

    updated font for jumbotron

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
+
 
 
 
