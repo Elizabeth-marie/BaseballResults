144484f6a58640d9583c5515125a91e56c6be265
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2019-05-12 19:35:38 -0600

    updated header to display year

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <nav id="nav" className="navbar navbar-dark navbar-fixed-top">
-    <span className="navbar-brand">MLB Standings</span>
+    <span className="navbar-brand">MLB Standings 2018</span>
   </nav>)
 }
 
