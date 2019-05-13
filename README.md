f9aaa5d48a46096e9adfcaa76574443f38e51cfc
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2019-05-12 20:17:26 -0600

    fixed readme to reflect dropdown changes

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 ## BaseballResults
 
 (https://baseball-results.herokuapp.com/)  
-An application for users to see MLB team standings for 2018 ranked by number of wins.
+An application for users to see MLB team standings for 2018 ranked by wins.
 Users can filter the results 3 ways:
 
-### 1. MLB
-The "MLB" selection will display all
-data ranked by wins.
+### 1. Division
 
-![MLB picture](./ReadMeImages/SelectionMLB.png "MLB Filter")
-
-### 2. Division
-
-The "Division" selection will display teams ranked by wins
-with the data filtered by division and by league.
+The "Division" selection will display teams ranked by wins and filtered by division and by league.
 
 ![MLB picture](./ReadMeImages/SelectionDivision.png "Division Filter")
 
-### 3. League
+### 2. League
 
  The "League" selection will
-display data filtered by league only with the teams ranked by wins within each
-league.
+display teams ranked by wins and filtered by league only.
 
-![AL picture](./ReadMeImages/SelectionLeague.png "AL Filter")   
+![AL picture](./ReadMeImages/SelectionLeague.png "AL Filter")
 
+### 3. MLB
+The "MLB" selection will display all
+teams ranked by wins.
+
+![MLB picture](./ReadMeImages/SelectionMLB.png "MLB Filter")  
 
 ## Getting Started
 
--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -3,63 +3,41 @@ import './Table.css'
 
 const Table = ({results, selectedValue, renderTableData}) => {
 
-//********************* MLB SELECTED ***********************
-if(selectedValue === "MLB") {
-  let ranked = results.sort(function(a, b){
-    return b.wins-a.wins
-  })
-  return (
-    <div className="container">
+  //********************* MLB SELECTED ***********************
+  if (selectedValue === "MLB") {
+    let ranked = results.sort(function(a, b) {
+      return b.wins - a.wins
+    })
+    return (<div className="container">
       <div>
         <h4 className="float-left resultHeadings">Major League Baseball</h4>
-       <table id="results">
-         <thead>
-           <tr>
-             <th>TEAM</th>
-             <th>WINS</th>
-             <th>LOSSES</th>
-           </tr>
-         </thead>
-         <tbody >
-           {renderTableData(ranked)}
-         </tbody>
-       </table>
-       </div>
+        <table id="results">
+          <thead>
+            <tr>
+              <th>TEAM</th>
+              <th>WINS</th>
+              <th>LOSSES</th>
+            </tr>
+          </thead>
+          <tbody >
+            {renderTableData(ranked)}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+    //******************* LEAGUE SELECTED ******************)
+  } else if (selectedValue === "League") {
+    let aLeague = results.filter(x => x.league === "AL").sort(function(a, b) {
+      return b.wins - a.wins
+    })
+    let nLeague = results.filter(x => x.league === "NL").sort(function(a, b) {
+      return b.wins - a.wins
+    })
 
-//******************* LEAGUE SELECTED ******************
-else if(selectedValue === "League") {
-  let aLeague = results.filter(x => x.league === "AL").sort(function(a, b){
-    return b.wins-a.wins
-  })
-  let nLeague = results.filter(x => x.league === "NL").sort(function(a, b){
-    return b.wins-a.wins
-  })
-
-  return (
-    <div>
+    return (<div>
       <div className="container">
         <div>
           <h4 className="float-left resultHeadings">American League</h4>
-         <table id="results">
-           <thead>
-             <tr>
-               <th>TEAM</th>
-               <th>WINS</th>
-               <th>LOSSES</th>
-             </tr>
-           </thead>
-           <tbody >
-             {renderTableData(aLeague)}
-           </tbody>
-         </table>
-         </div>
-         </div>
-         <div className="break"></div>
-         <div className="container">
-           <h4 className="float-left resultHeadings">National League</h4>
           <table id="results">
             <thead>
               <tr>
@@ -69,91 +47,105 @@ else if(selectedValue === "League") {
               </tr>
             </thead>
             <tbody >
-              {renderTableData(nLeague)}
+              {renderTableData(aLeague)}
             </tbody>
           </table>
-          </div>
+        </div>
+      </div>
+      <div className="break"></div>
+      <div className="container">
+        <h4 className="float-left resultHeadings">National League</h4>
+        <table id="results">
+          <thead>
+            <tr>
+              <th>TEAM</th>
+              <th>WINS</th>
+              <th>LOSSES</th>
+            </tr>
+          </thead>
+          <tbody >
+            {renderTableData(nLeague)}
+          </tbody>
+        </table>
+      </div>
 
     </div>
-  )
-}
-//************** DIVISION SELECTED *********************
-else if(selectedValue === "Division"){
-  let table1 = results.filter(x => x.league === "AL" && x.division === "East").sort(function(a, b){
-    return b.wins-a.wins
-  })
-  let table2 = results.filter(x => x.league === "AL" && x.division === "Central").sort(function(a, b){
-    return b.wins-a.wins
-  })
-  let table3 = results.filter(x => x.league === "AL" && x.division === "West").sort(function(a, b){
-    return b.wins-a.wins
-  })
-  let table4 = results.filter(x => x.league === "NL" && x.division === "East").sort(function(a, b){
-    return b.wins-a.wins
-  })
-  let table5 = results.filter(x => x.league === "NL" && x.division === "Central").sort(function(a, b){
-    return b.wins-a.wins
-  })
-  let table6 = results.filter(x => x.league === "NL" && x.division === "West").sort(function(a, b){
-    return b.wins-a.wins
-  })
+    //************** DIVISION SELECTED *********************)
+  } else if (selectedValue === "Division") {
+    let table1 = results.filter(x => x.league === "AL" && x.division === "East").sort(function(a, b) {
+      return b.wins - a.wins
+    })
+    let table2 = results.filter(x => x.league === "AL" && x.division === "Central").sort(function(a, b) {
+      return b.wins - a.wins
+    })
+    let table3 = results.filter(x => x.league === "AL" && x.division === "West").sort(function(a, b) {
+      return b.wins - a.wins
+    })
+    let table4 = results.filter(x => x.league === "NL" && x.division === "East").sort(function(a, b) {
+      return b.wins - a.wins
+    })
+    let table5 = results.filter(x => x.league === "NL" && x.division === "Central").sort(function(a, b) {
+      return b.wins - a.wins
+    })
+    let table6 = results.filter(x => x.league === "NL" && x.division === "West").sort(function(a, b) {
+      return b.wins - a.wins
+    })
 
-return (
-  <div>
-    <div className="container">
-      <div className="row justify-content-center">
-      <h4 className="leagueHeadings">American League</h4>
-    </div>
-      <h4 className="float-left resultHeadings">AL East</h4>
-     <table id="results">
-       <thead>
-         <tr>
-           <th>TEAM</th>
-           <th>WINS</th>
-           <th>LOSSES</th>
-         </tr>
-       </thead>
-       <tbody >
-         {renderTableData(table1)}
-       </tbody>
-     </table>
-     </div>
-     <div className="container">
-       <h4 className="float-left resultHeadings">AL Central</h4>
-      <table id="results">
-        <thead>
-          <tr>
-            <th>TEAM</th>
-            <th>WINS</th>
-            <th>LOSSES</th>
-          </tr>
-        </thead>
-        <tbody >
-          {renderTableData(table2)}
-        </tbody>
-      </table>
+    return (<div>
+      <div className="container">
+        <div className="row justify-content-center">
+          <h4 className="leagueHeadings">American League</h4>
+        </div>
+        <h4 className="float-left resultHeadings">AL East</h4>
+        <table id="results">
+          <thead>
+            <tr>
+              <th>TEAM</th>
+              <th>WINS</th>
+              <th>LOSSES</th>
+            </tr>
+          </thead>
+          <tbody >
+            {renderTableData(table1)}
+          </tbody>
+        </table>
       </div>
       <div className="container">
-          <h4 className="float-left resultHeadings">AL West</h4>
-       <table id="results">
-         <thead>
-           <tr>
-             <th>TEAM</th>
-             <th>WINS</th>
-             <th>LOSSES</th>
-           </tr>
-         </thead>
-         <tbody >
-           {renderTableData(table3)}
-         </tbody>
-       </table>
-       </div>
-       <div className="break"></div>
-       <div className="container">
-         <div className="row justify-content-center">
-         <h4 className="leagueHeadings">National League</h4>
-         </div>
-         <h4 className="float-left resultHeadings">NL East</h4>
+        <h4 className="float-left resultHeadings">AL Central</h4>
+        <table id="results">
+          <thead>
+            <tr>
+              <th>TEAM</th>
+              <th>WINS</th>
+              <th>LOSSES</th>
+            </tr>
+          </thead>
+          <tbody >
+            {renderTableData(table2)}
+          </tbody>
+        </table>
+      </div>
+      <div className="container">
+        <h4 className="float-left resultHeadings">AL West</h4>
+        <table id="results">
+          <thead>
+            <tr>
+              <th>TEAM</th>
+              <th>WINS</th>
+              <th>LOSSES</th>
+            </tr>
+          </thead>
+          <tbody >
+            {renderTableData(table3)}
+          </tbody>
+        </table>
+      </div>
+      <div className="break"></div>
+      <div className="container">
+        <div className="row justify-content-center">
+          <h4 className="leagueHeadings">National League</h4>
+        </div>
+        <h4 className="float-left resultHeadings">NL East</h4>
         <table id="results">
           <thead>
             <tr>
@@ -166,42 +158,40 @@ return (
             {renderTableData(table4)}
           </tbody>
         </table>
-        </div>
-        <div className="container">
-          <h4 className="float-left resultHeadings">NL Central</h4>
-         <table id="results">
-           <thead>
-             <tr>
-               <th>TEAM</th>
-               <th>WINS</th>
-               <th>LOSSES</th>
-             </tr>
-           </thead>
-           <tbody >
-             {renderTableData(table5)}
-           </tbody>
-         </table>
-         </div>
-         <div className="container">
-           <h4 className="float-left resultHeadings">NL West</h4>
-          <table id="results">
-            <thead>
-              <tr>
-                <th>TEAM</th>
-                <th>WINS</th>
-                <th>LOSSES</th>
-              </tr>
-            </thead>
-            <tbody >
-              {renderTableData(table6)}
-            </tbody>
-          </table>
-          </div>
-        </div>
-      )
-    }
+      </div>
+      <div className="container">
+        <h4 className="float-left resultHeadings">NL Central</h4>
+        <table id="results">
+          <thead>
+            <tr>
+              <th>TEAM</th>
+              <th>WINS</th>
+              <th>LOSSES</th>
+            </tr>
+          </thead>
+          <tbody >
+            {renderTableData(table5)}
+          </tbody>
+        </table>
+      </div>
+      <div className="container">
+        <h4 className="float-left resultHeadings">NL West</h4>
+        <table id="results">
+          <thead>
+            <tr>
+              <th>TEAM</th>
+              <th>WINS</th>
+              <th>LOSSES</th>
+            </tr>
+          </thead>
+          <tbody >
+            {renderTableData(table6)}
+          </tbody>
+        </table>
+      </div>
+    </div>)
+  }
 
 }
-
 
 export default Table
